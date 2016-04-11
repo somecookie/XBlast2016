f5119b3bfb3e774dd881a2eb2ff6cb553b9cad99
Clarisse Fleurimont <clarissefleurimont@tsf-444-wpa-3-022.epfl.ch>
2016-04-11 15:10:00 +0200

    Javadoc sur ArgumentChecker, Cell, Direction

--- XBlast2016/src/ch/epfl/xblast/Direction.java
+++ XBlast2016/src/ch/epfl/xblast/Direction.java
@@ -2,7 +2,12 @@ package ch.epfl.xblast;
 
 public enum Direction {
     N, E, S, W;
-    
+
+    /**
+     * Retourne la direction opposée de celle à laquelle on l'applique (S pour N, W pour E, etc.)
+     * @return opposite (Direction)
+     *          Direction opposée de celle à laquelle la méthode est appliquée
+     */
     public Direction opposite(){
         switch(this){
         case N:
@@ -17,14 +22,23 @@ public enum Direction {
            throw new Error();        
         } 
     }
-    
+
+    /**
+     * 
+     * @return
+     */
     public boolean isHorizontal(){
         if(this.equals(E) || this.equals(W)){
             return true;
         }
         return false;
     }
-    
+
+    /**
+     * 
+     * @param that
+     * @return
+     */
     public boolean isParallelTo(Direction that){
         if (that.equals(this) || that.equals(this.opposite())){
             return true;
